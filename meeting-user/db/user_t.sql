a3f7792934a8ea8d86e399f6318be93609ff18d1
leiming <2008weileiming@gmail.com>
2019-02-18 23:08:01 +0800

    Update sql

--- meeting-user/db/user_t.sql
+++ meeting-user/db/user_t.sql
@@ -5,7 +5,7 @@
  Source Server Type    : MySQL
  Source Server Version : 50724
  Source Host           : localhost:3306
- Source Schema         : guns_rest
+ Source Schema         : meeting_user
 
  Target Server Type    : MySQL
  Target Server Version : 50724
@@ -18,10 +18,10 @@ SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Table structure for mooc_user_t
+-- Table structure for user_t
 -- ----------------------------
-DROP TABLE IF EXISTS `mooc_user_t`;
-CREATE TABLE `mooc_user_t` (
+DROP TABLE IF EXISTS `user_t`;
+CREATE TABLE `user_t` (
   `UUID` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键编号',
   `user_name` varchar(50) DEFAULT NULL COMMENT '用户账号',
   `user_pwd` varchar(50) DEFAULT NULL COMMENT '用户密码',
@@ -40,11 +40,11 @@ CREATE TABLE `mooc_user_t` (
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='用户表';
 
 -- ----------------------------
--- Records of mooc_user_t
+-- Records of user_t
 -- ----------------------------
 BEGIN;
-INSERT INTO `mooc_user_t` VALUES (2, 'admin', '0192023a7bbd73250516f069df18b500', '隔壁泰山', 0, '2018-07-31', 'admin@mooc.com', '13888888888', '北京市海淀区朝阳北路中南海国宾馆', 'films/img/head-img.jpg', '没有合适的伞，我宁可淋着雨', 0, '2019-02-14 15:26:53', '2019-02-14 15:26:53');
-INSERT INTO `mooc_user_t` VALUES (3, 'jiangzh', '5e2de6bd1c9b50f6e27d4e55da43b917', '阿里郎', 0, '2018-08-20', 'jiangzh@mooc.com', '13866666666', '北京市朝阳区大望路万达广场', 'films/img/head-img.jpg', '我喜欢隔壁泰山', 1, '2019-02-14 15:26:53', '2019-02-14 15:26:53');
+INSERT INTO `user_t` VALUES (2, 'admin', '0192023a7bbd73250516f069df18b500', '隔壁泰山', 0, '2018-07-31', 'admin@mooc.com', '13888888888', '北京市海淀区朝阳北路中南海国宾馆', 'films/img/head-img.jpg', '没有合适的伞，我宁可淋着雨', 0, '2019-02-14 15:26:53', '2019-02-14 15:26:53');
+INSERT INTO `user_t` VALUES (3, 'jiangzh', '5e2de6bd1c9b50f6e27d4e55da43b917', '阿里郎', 0, '2018-08-20', 'jiangzh@mooc.com', '13866666666', '北京市朝阳区大望路万达广场', 'films/img/head-img.jpg', '我喜欢隔壁泰山', 1, '2019-02-14 15:26:53', '2019-02-14 15:26:53');
 COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
