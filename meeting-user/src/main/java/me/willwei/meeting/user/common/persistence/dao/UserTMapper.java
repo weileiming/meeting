ba782883f955487eddd41536240f472eb5285992
leiming <2008weileiming@gmail.com>
2019-02-20 16:45:46 +0800

    Generate user dao

---
+++ meeting-user/src/main/java/me/willwei/meeting/user/common/persistence/dao/UserTMapper.java
@@ -0,0 +1,16 @@
+package me.willwei.meeting.user.common.persistence.dao;
+
+import me.willwei.meeting.user.common.persistence.model.UserT;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 用户表 Mapper 接口
+ * </p>
+ *
+ * @author leiming
+ * @since 2019-02-20
+ */
+public interface UserTMapper extends BaseMapper<UserT> {
+
+}
--- meeting-user/src/main/java/me/willwei/meeting/user/modular/user/service/UserServiceImpl.java
+++ meeting-user/src/main/java/me/willwei/meeting/user/modular/user/service/UserServiceImpl.java
@@ -1,10 +1,13 @@
 package me.willwei.meeting.user.modular.user.service;
 
+import com.stylefeng.guns.core.util.MD5Util;
 import me.willwei.meeting.api.user.UserModel;
 import me.willwei.meeting.api.user.UserService;
 import me.willwei.meeting.api.user.UserVO;
+import me.willwei.meeting.user.common.persistence.dao.UserTMapper;
+import me.willwei.meeting.user.common.persistence.model.UserT;
 import org.apache.dubbo.config.annotation.Service;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * UserServiceImpl
@@ -15,17 +18,32 @@ import org.springframework.beans.factory.annotation.Value;
 @Service(version = "${user.service.version}")
 public class UserServiceImpl implements UserService {
 
-    @Value("${dubbo.application.name}")
-    private String serviceName;
+    @Autowired
+    private UserTMapper userTMapper;
 
     @Override
     public int login(String username, String password) {
-        System.out.println(serviceName + ": " + "this is user service: " + username + ", " + password);
         return 0;
     }
 
     @Override
     public boolean register(UserModel userModel) {
+        // 将注册信息实体转换为数据实体
+        UserT userT = new UserT();
+        userT.setUserName(userModel.getUsername());
+        userT.setUserPhone(userModel.getPhone());
+        userT.setEmail(userModel.getEmail());
+        userT.setAddress(userModel.getAddress());
+        // 创建时间和修改时间 -> CURRENT_TIMESTAMP
+        // 数据加密【MD5混淆加密 + 盐值 -> Shiro加密】
+        String md5Pwd = MD5Util.encrypt(userModel.getPassword());
+        userT.setUserPwd(md5Pwd);
+
+        // 将数据实体存入数据库
+        Integer insert = this.userTMapper.insert(userT);
+        if (insert > 0) {
+            return true;
+        }
         return false;
     }
 
